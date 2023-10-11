ae8791deff4a3bf83ab4fe4c88eeb4e66adc558f
amrki <amrkon01@fesb.hr>
2023-10-11 18:33:34 +0200

    Added lab1

--- lab1/lab1/Source.c
+++ lab1/lab1/Source.c
@@ -1,9 +1,43 @@
+﻿/*. Napisati program koji prvo procita koliko redaka ima datoteka, tj. koliko ima studenata
+zapisanih u datoteci. Nakon toga potrebno je dinamicki alocirati prostor za niz struktura
+studenata (ime, prezime, bodovi) i ucitati iz datoteke sve zapise. Na ekran ispisati ime,
+prezime, apsolutni i relativni broj bodova.
+Napomena: Svaki redak datoteke sadrzi ime i prezime studenta, te broj bodova na kolokviju.
+relatvan_br_bodova = br_bodova/max_br_bodova*100*/
+
+
 #include <stdio.h>
+#define MAX_SIZE 50
+#define MAX_LINE 1024
 
-int main()
-{
+typedef struct _student {
+	char name[MAX_SIZE];
+	char surname[MAX_SIZE];
+	double points;
+}Student;
 
-	printf("Hello World");
+int main(){
 
+	int noRows = 0;
+
+	FILE *filePointer = NULL;
+
+	char buffer[MAX_LINE] = { 0 };
+
+	filePointer = fopen("students.txt", "r");
+	if (!filePointer) {
+		printf("File is not open!\n");
+
+		return -1;
+	}
+
+	while (!feof(filePointer)) {
+		fgets(buffer, MAX_LINE, filePointer);
+		noRows++;
+	}
+
+	printf("%d", noRows);
+
+	fclose(filePointer);
 	return 0;
 }
